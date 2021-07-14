f13f62bf720c7c3b878c43d20ee0001dd0cf43a1
smcommits <sm.anas@outlook.com>
2021-07-14 23:20:54 +0530

    Add Profile Icon, Change button hover
    Added profile icon in the navbar. The image is svg and the border is applied to the parent container
    
    Added cursor to pointer on interactive links with class="small_text". y

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,13 +3,20 @@ import HorizontalList from '../styles/StyledComponents';
 
 const Nav = () => (
   <header>
-    <nav className="flex_row_center">
-      <h1 className="text_logo">Bookstore CMS</h1>
-      <HorizontalList className="main_nav">
-        <li className="text_nav_active">Home</li>
-        <li className="text_nav">Categories</li>
-      </HorizontalList>
+    <nav className="flex_row_center space_between">
+      <div className="left flex_row_center">
+        <h1 className="text_logo">Bookstore CMS</h1>
+        <HorizontalList className="main_nav">
+          <li className="text_nav_active">Home</li>
+          <li className="text_nav">Categories</li>
+        </HorizontalList>
+      </div>
+
+      <div className="right">
+        <img src="/user.svg" alt="profile-icon" className="profile_image" />
+      </div>
     </nav>
+
   </header>
 );
 
